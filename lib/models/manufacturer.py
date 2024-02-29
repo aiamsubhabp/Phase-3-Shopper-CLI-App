3a39b62b888b9c5d6add9f5f779d0df7b5a179c3
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2024-02-28 22:49:19 -0500

    create getters and setters for manufacturer class properties

--- lib/models/manufacturer.py
+++ lib/models/manufacturer.py
@@ -8,3 +8,32 @@ class Manufacturer:
         self.id = id
         self._name = name
         self._industry = industry
+        
+    def __repr__(self):
+        pass
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError(
+                'Manufacturer name must be a non-empty string'
+            )
+    
+    @property
+    def industry(self):
+        return self._industry
+    
+    @industry.setter
+    def industry(self, industry):
+        if isinstance(industry, str) and len(industry):
+            self._industry = industry
+        else:
+            raise ValueError(
+                'Industry must be a non-empty string'
+            )
