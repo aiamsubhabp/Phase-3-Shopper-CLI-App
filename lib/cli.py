c15d02b52bdac3c8ee8df8ccd46bfe070ecf6d48
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2024-03-01 15:30:40 -0500

    add helper functions for Product ORM. finish menu in cli.pu

--- lib/cli.py
+++ lib/cli.py
@@ -9,7 +9,14 @@ from helpers import (
     find_manufacturer_by_name,
     create_manufacturer,
     update_manufacturer,
-    delete_manfacturer
+    delete_manfacturer,
+    list_products,
+    find_product_by_name,
+    find_product_by_id,
+    create_product,
+    update_product,
+    delete_product,
+    list_manufacturer_products
 )
 
 def greeting():
@@ -24,13 +31,29 @@ def main():
         elif choice == "1":
             list_manufacturers()
         elif choice == "2":
-            list_manufacturers()
+            find_manufacturer_by_name()
         elif choice == "3":
-            list_manufacturers()
+            find_manufacturer_by_id()
         elif choice == "4":
-            list_manufacturers()
+            create_manufacturer()
         elif choice == "5":
-            list_manufacturers()           
+            update_manufacturer()        
+        elif choice == "6":
+            delete_manfacturer()    
+        elif choice == "7":
+            list_products()
+        elif choice == "8":
+            find_product_by_name()
+        elif choice == "9":
+            find_product_by_id()
+        elif choice == "10":
+            create_product()
+        elif choice == "11":
+            update_product()        
+        elif choice == "12":
+            delete_product() 
+        elif choice == "13":
+            list_manufacturer_products() 
         else:
             print("Invalid choice")
 
@@ -38,14 +61,20 @@ def main():
 def menu():
     print("Please select an option:")
     print("0. Exit the program")
-    print("1. Some useful function")
-    print("2. Some useful function")
-    print("3. Some useful function")
-    print("4. Some useful function")
-    print("5. Some useful function")
-    print("6. Some useful function")
-    print("7. Some useful function")
-    print("8. Some useful function")
+    print("1. List all manufacturers")
+    print("2. Find manufacturer by name")
+    print("3. Find manufacturer by id")
+    print("4. Create new manufacturer")
+    print("5. Update existing manufacturer")
+    print("6. Delete manufacturer")
+    print("7. List all products")
+    print("8. Find product by name")
+    print("9. Find product by id")
+    print('10. Create new product')
+    print('11. Update existing product')
+    print('12. Delete product')
+    print('13. List all products by specified manufacturer')
+
 
 
 
--- lib/helpers.py
+++ lib/helpers.py
@@ -58,4 +58,69 @@ def delete_manfacturer():
     else:
         print(f'Manufacturer {mfg_id} not found')
 
+def list_products():
+    products = Product.get_all()
+    for product in products:
+        print(product)
+
+def find_product_by_name():
+    name = input("Enter the product's name: ")
+    product = Product.find_by_name(name)
+    print(product) if product else print(f'{product} not found')
+
+def find_product_by_id():
+    product_id = input("Enter the product id: ")
+    product = Product.find_by_id(product_id)
+    print(product) if product else print(f'Product {product_id} not found')
+
+def create_product():
+    name = input("Enter the products name: ")
+    product_type = input("Enter the product type: ")
+    mfg_id = input("Enter the product's manufacturer id: ")
+    try:
+        product = Product.create(name, product_type, mfg_id)
+        print(f'Success: {product}')
+    except Exception as exc:
+        print("Error creating product", exc)
+
+def update_product():
+    product_id = input("Enter the product id: ")
+    if product := Product.find_by_id(product_id):
+        try:
+            name = input("Enter the product's new name: ")
+            product.name = name
+            product_type = input("Enter the product's updated type(if unchanged, enter the previous product type)")
+            product.product_type = product_type
+            mfg_id = input("Enter the product's new manufacturer id (if unchanged, enter the previous manufacturer id)")
+            product.manufacturer_id = mfg_id
+
+            product.update()
+            print(f'Success: {product}')
+        except Exception as exc:
+            print("Error upating product: ", exc)
+    else:
+        print(f'Product {product_id} not found')
+
+def delete_product():
+    product_id = input("Enter the product id: ")
+    if product := Product.find_by_id(product_id):
+        product.delete()
+        print(f'Product {product_id} deleted')
+    else:
+        print(f'Product {product_id} not found')
+
+def list_manufacturer_products():
+    mfg_id = input("Enter the manufacturer id: ")
+    manufacturer = Manufacturer.find_by_id(int(mfg_id))
+    if manufacturer:
+        products = manufacturer.products()
+        if products:
+            print(f'Products manufacturered by {manufacturer.name}: ')
+            for product in products:
+                print(f'Product id: {product.id}, Name: {product.name}')
+        else:
+            print(f"No products found under {manufacturer.name}")
+    else:
+        print(f'Manufacturer with id: {mfg_id} not found in the database')
+
 
