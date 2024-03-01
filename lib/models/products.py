513a9806c11d6b6177ec15adba610c0347258ed0
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2024-02-29 19:48:40 -0500

    add name, product_type, and manufacturer_id properties to Product class

--- lib/models/products.py
+++ lib/models/products.py
@@ -16,5 +16,44 @@ class Product:
             f"<Product {self.id}: {self.name}, {self.product_type}, " + 
             f"Manufacturer ID: {self.manufacturer_id}>"
         )
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError(
+                'Name must be a non-empty string'
+            )
+    
+    @property
+    def product_type(self):
+        return self._product_type
+    
+    @product_type.setter
+    def product_type(self, product_type):
+        if isinstance(product_type, str) and len(product_type):
+            self._product_type = product_type
+        else:
+            raise ValueError(
+                'Product type must be a non-empty string'
+            )
+    
+    @property
+    def manufacturer_id(self):
+        return self._manufacturer_id
+    
+    @manufacturer_id.setter
+    def manufacturer_id(self, manufacturer_id):
+        if isinstance(manufacturer_id, int) and Manufacturer.find_by_id(manufacturer_id):
+            self._manufacturer_id = manufacturer_id
+        else:
+            raise ValueError(
+                'manufacturer id must reference a manufacturer in the database'
+            )
 
 
